7b112066c4326d316cf7835b3fb1108a6aeabae4
teapot-4l8 <chaiyunze@gmail.com>
2024-05-16 19:26:50 +0800

    图片遗忘加了echarts但布局乱了

--- src/page/Company/Advice.vue
+++ src/page/Company/Advice.vue
@@ -9,7 +9,13 @@
     </div>
 
     <div class="row">
-        <div id="chart2" ref="chart2" style="width: 1080px; height: 300px;"></div>
+        <div class="col">
+            <div class="chart-container" style="display: flex;">
+                <div id="chart2" ref="chart2" style="width: 50%; height: 300px;"></div>
+                <div id="radar2" ref="radar2" style="width: 50%; height: 300px;"></div>
+            </div>
+        </div>
+        
     </div>
 
     <div class="row">
@@ -30,6 +36,7 @@ const chart1 = ref(null); // 部分遗忘 4个方法正确率对比
 const chart2 = ref(null); 
 const chart3 = ref(null); 
 const radar1 = ref(null);
+const radar2 = ref(null);
 
 onMounted(() => {
     if(chart1.value) {
@@ -180,7 +187,7 @@ onMounted(() => {
         var option = {
             color: ['#67F9D8', '#FFE434', '#56A3F1', '#FF917C'],
             title: {
-                text: '类别遗忘'
+                text: '部分遗忘'
             },
             legend: {},
             radar: [
@@ -294,6 +301,112 @@ onMounted(() => {
                         }
                         ])
                     }
+                    }
+                ]
+                }
+            ]
+            };
+        myChart.setOption(option);
+    }
+
+    if(radar2.value) {
+        const myChart = echarts.init(radar2.value)
+        var option = {
+            title: {
+                text: '类别遗忘'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                left: 'center',
+                data: [
+                'A Software',
+                'A Phone',
+                'Another Phone',
+                'Precipitation',
+                'Evaporation'
+                ]
+            },
+            radar: [
+                {
+                indicator: [
+                    { text: 'Brand', max: 100 },
+                    { text: 'Content', max: 100 },
+                    { text: 'Usability', max: 100 },
+                    { text: 'Function', max: 100 }
+                ],
+                center: ['25%', '40%'],
+                radius: 80
+                },
+                {
+                indicator: [
+                    { text: 'Look', max: 100 },
+                    { text: 'Photo', max: 100 },
+                    { text: 'System', max: 100 },
+                    { text: 'Performance', max: 100 },
+                    { text: 'Screen', max: 100 }
+                ],
+                radius: 80,
+                center: ['50%', '60%']
+                },
+                {
+                indicator: (function () {
+                    var res = [];
+                    for (var i = 1; i <= 12; i++) {
+                    res.push({ text: i + '月', max: 100 });
+                    }
+                    return res;
+                })(),
+                center: ['75%', '40%'],
+                radius: 80
+                }
+            ],
+            series: [
+                {
+                type: 'radar',
+                tooltip: {
+                    trigger: 'item'
+                },
+                areaStyle: {},
+                data: [
+                    {
+                    value: [60, 73, 85, 40],
+                    name: 'A Software'
+                    }
+                ]
+                },
+                {
+                type: 'radar',
+                radarIndex: 1,
+                areaStyle: {},
+                data: [
+                    {
+                    value: [85, 90, 90, 95, 95],
+                    name: 'A Phone'
+                    },
+                    {
+                    value: [95, 80, 95, 90, 93],
+                    name: 'Another Phone'
+                    }
+                ]
+                },
+                {
+                type: 'radar',
+                radarIndex: 2,
+                areaStyle: {},
+                data: [
+                    {
+                    name: 'Precipitation',
+                    value: [
+                        2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 75.6, 82.2, 48.7, 18.8, 6.0, 2.3
+                    ]
+                    },
+                    {
+                    name: 'Evaporation',
+                    value: [
+                        2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 35.6, 62.2, 32.6, 20.0, 6.4, 3.3
+                    ]
                     }
                 ]
                 }
